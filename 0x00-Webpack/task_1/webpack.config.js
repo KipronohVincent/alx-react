4165a4b031bb2eba46b775f9b2db4b1bf8483b51
KipronohVincent <kipronohvincent43@gmail.com>
2023-01-23 13:03:30 +0300

    is updated

--- 0x00-Webpack/task_1/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: 'bundle.js',
-  }
-};
