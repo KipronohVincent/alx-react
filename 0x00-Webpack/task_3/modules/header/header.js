d48a4a0db2f2522f05658e264aae9148bd83ac4b
KipronohVincent <kipronohvincent43@gmail.com>
2023-01-23 17:43:27 +0300

    header

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import img from "../../assets/holberton-logo.jpg"
+import "../footer/footer.css";
+
+$('body').append("<div id='logo'></div>");
+$('body').append("<h1>Holberton Dashboard</h1>");
+
+console.log("Init header");
+
