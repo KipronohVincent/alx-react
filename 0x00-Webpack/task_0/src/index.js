340fe68b7695f5a20ffa6e5b174c604250e9c00f
KipronohVincent <kipronohvincent43@gmail.com>
2023-01-23 16:04:15 +0300

    removed

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-
-$(document).ready(function(){
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
-});
