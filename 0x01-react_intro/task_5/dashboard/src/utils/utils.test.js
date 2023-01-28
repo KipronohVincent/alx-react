c28bc5626b8a794a95200a8432b95cb1ceb5ce5d
KipronohVincent <kipronohvincent@gmail.com>
2023-01-28 21:50:36 +0300

    is updated

--- 0x01-react_intro/task_5/dashboard/src/utils/utils.test.js
+++ 0x01-react_intro/task_5/dashboard/src/utils/utils.test.js
@@ -2,7 +2,7 @@ import { getFullYear, getFooterCopy, getLatestNotification } from './utils'
 
 test('Check that the function returns the correct year', () => {
     const year = getFullYear();
-    expect(year).toEqual(2022)
+    expect(year).toEqual(2023)
   });
   
   test('Check that returns the correct string when the argument is true or false', () => {
